77bd1afc55cf4e938a3247f5f6513716c11178c3
Indra Wijaya S <Wijayaindra555@gmail.com>
2023-06-02 18:00:47 +0700

    Belajar Java Standared Classes versi 3

---
+++ src/indra/classes/MathApp.java
@@ -0,0 +1,12 @@
+package indra.classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+        var min = Math.min(100,500);
+        System.out.println(min);
+        var max = Math.max(100,500);
+        System.out.println(max);
+        System.out.println(Math.PI);
+    }
+}
---
+++ src/indra/classes/NumberApp.java
@@ -0,0 +1,18 @@
+package indra.classes;
+
+public class NumberApp {
+    public static void main(String[] args) {
+
+        Integer intValue = 10;
+
+        Long longValue = intValue.longValue();
+        Double doubleValue = longValue.doubleValue();
+        Short shortValue = doubleValue.shortValue();
+
+        String contoh = "10000";
+
+        Integer contohInt = Integer.valueOf(contoh);
+
+        System.out.println(contohInt);
+    }
+}
